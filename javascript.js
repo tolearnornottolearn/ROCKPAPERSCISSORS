0af792eb7d7ddb101550790dd15acbb051c8404b
Ville <ville.hall@gmail.com>
2024-08-19 16:02:05 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+console.log("Hello World")
+
+function getComputerChoice
+
+
+function playRound(humanChoice, computerChoice) {
+    // your code here!
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
